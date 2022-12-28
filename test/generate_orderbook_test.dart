78b472ef32a1e2cb549f6b124b884ee1c1c6f452
MusketPosition <musket.gun@position.exchange>
2022-12-28 13:33:28 +0700

    fix bug

--- test/generate_orderbook_test.dart
+++ test/generate_orderbook_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:position_dex_sdk/position_dex_sdk.dart';
@@ -38,16 +37,14 @@ void main() {
     //   var a=1;
     // });
 
-
-
     test("generateDepth-liquidity-bid", () {
       LiquidityAction liquidity = LiquidityAction();
 
       List<List<String>> output = generateDepth(
           [
-            ["8.4000","2"],
-            ["8.1500","10"],
-            ["7.0000","10"]
+            ["8.4000", "2"],
+            ["8.1500", "10"],
+            ["7.0000", "10"]
           ].cast<List<String>>(),
           TypeDepth.bid,
           10000,
@@ -58,17 +55,14 @@ void main() {
               basisPoint: 10000,
               currentPip: 84789,
               liquidityIndex: [
-                ["2","3775.83395298","32014.96198738"]
+                ["2", "3775.83395298", "32014.96198738"]
               ].toList()));
 
-      var a=1;
-
+      var a = 1;
 
       output.forEach((element) {
         print(' $element');
       });
-
-
     });
 
     test("generateDepth-liquidity-ask", () {
@@ -76,9 +70,9 @@ void main() {
 
       List<List<String>> output = generateDepth(
           [
-            ["8.4000","2"],
-            ["8.1500","10"],
-            ["7.0000","10"]
+            ["8.4000", "2"],
+            ["8.1500", "10"],
+            ["7.0000", "10"]
           ].cast<List<String>>(),
           TypeDepth.ask,
           1,
@@ -89,24 +83,22 @@ void main() {
               basisPoint: 10000,
               currentPip: 68989,
               liquidityIndex: [
-                ["2","3775.83395298","26049.1008582"]
+                ["2", "3775.83395298", "26049.1008582"]
               ].toList()));
 
-      var a=1;
-
+      var a = 1;
 
       output.forEach((element) {
         print(' $element');
       });
-
-
     });
     test("generateDepth-liquidity-camel/busd", () {
       LiquidityAction liquidity = LiquidityAction();
 
       List<List<String>> output = generateDepth(
           [
-            ["65000","715.40916085"],["64900","5000"]
+            ["65000", "715.40916085"],
+            ["64900", "5000"]
           ].cast<List<String>>(),
           TypeDepth.bid,
           100,
@@ -117,19 +109,37 @@ void main() {
               basisPoint: 10000,
               currentPip: 61000,
               liquidityIndex: [
-                ["0","219.27326278","657.79786102"],
-                ["1","69.99395276","419.95671715"],
-                ["2","100","610"],
-                ["3","918.10922695","8262.98304252"]
+                ["0", "219.27326278", "657.79786102"],
+                ["1", "69.99395276", "419.95671715"],
+                ["2", "100", "610"],
+                ["3", "918.10922695", "8262.98304252"]
               ].toList()));
-
 
       output.forEach((element) {
         print(' $element');
       });
-
-
     });
 
+    test("generateDepth-liquidity-posi/light", () {
+      LiquidityAction liquidity = LiquidityAction();
+
+      List<List<String>> output = generateDepth(
+          [].cast<List<String>>(),
+          TypeDepth.bid,
+          1,
+          LiquidityPair(
+              currentIndexPipRange: 2,
+              pipRange: 30000,
+              pipSpace: 300,
+              basisPoint: 10000,
+              currentPip: 63000,
+              liquidityIndex: [
+                ["2", "606.1150569144137089", "3818.5248690959376642"]
+              ].toList()));
+
+      output.forEach((element) {
+        print(' $element');
+      });
+    });
   });
 }
